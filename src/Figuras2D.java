213965f140c4f1f025f772d06940e707927b8618
Juan Diego <juandiegojd252@gmail.com>
2023-06-06 07:31:37 -0500

    añadir dos archivos iniciales Creacion del constructor, setters y getters en la clase figuras2d

--- src/Figuras2D.java
+++ src/Figuras2D.java
@@ -1,3 +1,19 @@
 public class Figuras2D {
+    public Figuras2D(int num_lados) {
+        this.num_lados = num_lados;
+    }
+
+    public int getNum_lados() {
+        return num_lados;
+    }
+
+    public void setNum_lados(int num_lados) {
+        this.num_lados = num_lados;
+    }
+
+    int num_lados;
+    areaCuadrado(){
+
+    }
 
 }
